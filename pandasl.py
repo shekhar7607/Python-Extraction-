857235d9ea2435f3b96fe67ba6add0a5575903c6
swami <shekhar7607@gmail.com>
2026-06-16 09:59:30 +0530

    Updated Python file

---
+++ pandasl.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+a = [1, 7, 9]
+
+myvar  = pd.Series(a, index = ["x","y","z"])
+
+
+print(myvar)
--- text.py
+++ text.py
@@ -3,20 +3,15 @@ import pandas as pd
 
 df = pd.read_excel("trip.xlsx")
 
-total_unique_trips = df['trip ID'].nunique()
-print(f"Total unique trips: {total_unique_trips}")
+
+escort_trip = len(df[df["escort"] == "yes"])
+
+print(f"Number of escort trips: {escort_trip}")
+
+count =0
+for name in df['name']:
+    if name[:2] == 'sw':
+        count += 1
 
 
-print("gender counts:")
-print(df["gender"].value_counts())
-
-
-print(f"Total Employees: {df['empid'].nunique()}")
-
-
-escort_trips = df[df['escort'] == 'no'].count()['trip ID']
-print(f"Total No Escort Trips: {escort_trips}")
-
-female_employee = df[df['escort'] == 'yes'][['empid','name']]
-
-print(female_employee)
+print(f"Number of trips with names starting with 'Sw': {count}")
